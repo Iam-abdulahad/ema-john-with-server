b77305e5041073bcbd56af816bcd4b682cd6b9b3
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-01-23 09:17:07 +0600

    signup page designed

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './SignIn.css';
 
-const SignIn = () => {
+const SignUp = () => {
           return (
                     <div className='signin-card'>
                               <div>
-                                        <h4 className="signin-text">SignIn</h4>
+                                        <h4 className="signin-text">SignUp</h4>
                                         <form>
                                                   <div className='input-group'>
                                                             <label htmlFor="Email">Email</label>
                                                             <input type="email" name="Your Email" id="" required/>
                                                             <label htmlFor="Password">Password</label>
                                                             <input type="password" name="password" id="" required/>
+                                                            <label htmlFor="Confirm-Password">Confirm Password</label>
+                                                            <input type="password" name="confirm-password" id="" required/>
                                                   </div>
                                                   <button className='btn-submit' type="submit">Sign in</button>
                                                   <p>
-                                                            New to Ema-John? <Link className='new-ac-link' to='/signup'>Create a new Account</Link>
+                                                            Already have an account? <Link className='new-ac-link' to='/signin'>Please Login</Link>
                                                   </p>
                                         </form>
                               </div>
@@ -24,4 +25,4 @@ const SignIn = () => {
           );
 };
 
-export default SignIn;
+export default SignUp;
