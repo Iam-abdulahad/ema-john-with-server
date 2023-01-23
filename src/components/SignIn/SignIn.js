42dd8e2c66330871efa719ccbe3110beb25e4614
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-01-23 15:07:58 +0600

    sign in method worked

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,19 +1,55 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './SignIn.css';
 
 const SignIn = () => {
+
+          const [email, setEmail] = useState('');
+          const [password, setPassword] = useState('');
+          const [
+                    signInWithEmailAndPassword,
+                    user,
+                    loading,
+                    error,
+          ] = useSignInWithEmailAndPassword(auth);
+
+          const navigate = useNavigate();
+
+          const hnadleEmailBlur = event => {
+                    setEmail(event.target.value);
+          }
+
+          const handlePasswordBlur = event => {
+                    setPassword(event.target.value);
+          }
+
+          if (user) {
+                    navigate('/shop')
+          }
+
+
+          const handleUserSignIn = event => {
+                    event.preventDefault()
+                    signInWithEmailAndPassword(email, password);
+          }
           return (
                     <div className='signin-card'>
                               <div>
                                         <h4 className="signin-text">SignIn</h4>
-                                        <form>
+                                        <form onSubmit={handleUserSignIn}>
                                                   <div className='input-group'>
                                                             <label htmlFor="Email">Email</label>
-                                                            <input type="email" name="Your Email" id="" required/>
+                                                            <input onBlur={hnadleEmailBlur} type="email" name="Your Email" id="" required />
                                                             <label htmlFor="Password">Password</label>
-                                                            <input type="password" name="password" id="" required/>
+                                                            <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
                                                   </div>
+                                                  <p>{error?.message}</p>
+                                                  {
+                                                            loading && <p>loading...</p>
+                                                  }
                                                   <button className='btn-submit' type="submit">Sign in</button>
                                                   <p>
                                                             New to Ema-John? <Link className='new-ac-link' to='/signup'>Create a new Account</Link>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -11,7 +11,7 @@ const SignUp = () => {
           const [confirmPassword, setConfirmPassword] = useState('');
           const [error, setError] = useState('');
           const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
-          const navigate = useNavigate;
+          const navigate = useNavigate();
 
 
           const handleEmailBlur = event => {
@@ -25,15 +25,15 @@ const SignUp = () => {
           const handleConfirmPasswordBlur = event => {
                     setConfirmPassword(event.target.value);
           }
-          
-          if (user){
-                    return(
-                              navigate('/shop')
-                    )
+
+          if (user) {
+
+                    navigate('/shop')
+
           }
           const handleCreateUser = event => {
                     event.preventDefault()
-                    if(password !== confirmPassword){
+                    if (password !== confirmPassword) {
                               setError('Your password are not matched! Please type similar password in both field.')
                               return;
                     }
@@ -49,11 +49,11 @@ const SignUp = () => {
                                                             <input onBlur={handleEmailBlur} type="email" name="Your Email" id="" required />
                                                             <label htmlFor="Password">Password</label>
                                                             <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
-                                                            <p style={{color: 'red'}}>{error}</p>
+                                                            <p style={{ color: 'red' }}>{error}</p>
                                                             <label htmlFor="Confirm-Password">Confirm Password</label>
                                                             <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
                                                   </div>
-                                                  <button onClick={() => {createUserWithEmailAndPassword(email, password)}} className='btn-submit' type="submit">Sign up</button>
+                                                  <button onClick={() => { createUserWithEmailAndPassword(email, password) }} className='btn-submit' type="submit">Sign up</button>
                                                   <p>
                                                             Already have an account? <Link className='new-ac-link' to='/signin'>Please Login</Link>
                                                   </p>
