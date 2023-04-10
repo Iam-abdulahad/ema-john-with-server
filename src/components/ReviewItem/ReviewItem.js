4e927553c3ffd30b85eb1d3a7ff34907bf59c180
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-04-10 11:59:18 +0600

    final commit

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,11 +1,11 @@
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import './ReviewItem.css'; 
+import './ReviewItem.css';
 
 const ReviewItem = (props) => {
-    const {product, handleRemoveProduct} = props;
-    const {name, img, price, shipping, quantity} = product;
+    const { product, handleRemoveProduct } = props;
+    const { name, img, price, shipping, quantity } = product;
     return (
         <div className='review-item'>
             <div>
@@ -14,7 +14,7 @@ const ReviewItem = (props) => {
             <div className="review-item-details-container">
                 <div className="review-item-details">
                     <p className="product-name" title={name}>
-                        { name.length > 20 ? name.slice(0, 20) + '...': name }
+                        {name.length > 20 ? name.slice(0, 20) + '...' : name}
                     </p>
                     <p>Price: <span className='orange-color'>${price}</span></p>
                     <p><small>Shipping: ${shipping}</small></p>
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -4,10 +4,10 @@ import auth from '../../firebase.init';
 
 const Shipment = () => {
           const [user] = useAuthState(auth);
-          const [email, setEmail] = useState('');
           const [name, setName] = useState('');
-          const [phone, setPhone] = useState('');
+          const [email, setEmail] = useState('');
           const [address, setAddress] = useState('');
+          const [phone, setPhone] = useState('');
           const [error, setError] = useState('');
           // const navigate = useNavigate();
 
@@ -15,32 +15,45 @@ const Shipment = () => {
                     setName(event.target.value);
           }
 
+          const handleAddressBlur = event => {
+                    setAddress(event.target.value);
+          }
+
           const handlePhoneBlur = event => {
                     setPhone(event.target.value);
           }
 
-          const handleAddressBlur = event => {
-                    setAddress(event.target.value);
+          const handleCreateUser = event => {
+                    event.preventDefault();
+                    const shipping = { name, email, address, phone };
+                    console.log(shipping);
           }
+
           return (
-                    <div className='signin-card'>
+                    <div className='form-container'>
                               <div>
-                                        <h4 className="signin-text">Shipping Info</h4>
-                                        <form s>
-                                                  <div className='input-group'>
-                                                            <label htmlFor="text">Your Name</label>
-                                                            <input onBlur={handleNameBlur} type="text" name="Your Email" id="" required />
-                                                            <label htmlFor="Email">Your Email</label>
-                                                            <input value= {user?.email} readOnly type="email" name="Your Email" id="" required />
-                                                            <label htmlFor="number">Your Phone</label>
-                                                            <input onBlur={handlePhoneBlur} type="number" name="number" id="" required />
-                                                            <p style={{ color: 'red' }}>{error}</p>
-                                                            <label htmlFor="address">Address</label>
+                                        <h2 className='form-title'>Your Shipping Info</h2>
+                                        <form onSubmit={handleCreateUser}>
+                                                  <div className="input-group">
+                                                            <label htmlFor="name">Your Name</label>
+                                                            <input onBlur={handleNameBlur} type="text" name="name" id="" required />
+                                                  </div>
+                                                  <div className="input-group">
+                                                            <label htmlFor="email">Your Email</label>
+                                                            <input value={user?.email} readOnly type="email" name="email" id="" required />
+                                                  </div>
+                                                  <div className="input-group">
+                                                            <label htmlFor="password">Address</label>
                                                             <input onBlur={handleAddressBlur} type="text" name="address" id="" required />
                                                   </div>
-                                                  <button className='btn-submit' type="submit">Confirm Order</button>
-
+                                                  <div className="input-group">
+                                                            <label htmlFor="phone">Phone Number</label>
+                                                            <input onBlur={handlePhoneBlur} type="text" name="phone" id="" required />
+                                                  </div>
+                                                  <p style={{ color: 'red' }}>{error}</p>
+                                                  <input className='form-submit' type="submit" value="Add Shipping" required />
                                         </form>
+
                               </div>
                     </div>
           );
